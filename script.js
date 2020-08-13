c2d6f1be5dbb8b1ae71a473793dc0be372c68576
totoi690 <tozaomello@gmail.com>
2020-08-13 14:22:30 -0300

    arrumação do estilo
    ainda sem conseguir usar o LocalStorage

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let tarefas = [
-    'Terminar a lista do Vini',
-    'Tarefa Rodriguinho (Ap. 6) (601, 603, 606, 609, 611, 615, 619, 620)',
-    'Tarefa Renan (Ap. 5) (574, 570, 571, 577, 579, 563 ,565)',
+    'Tarefa Lucas (Ap. 5) (561, 563, 565, 570, 574, 580)',
+    'Tarefa Renan (Ap. 6) (581, 594, 584, 586 e 582)',
+    'Tarefa Rose (Ap. 6) (623, 629, 630, 634, 636 e 639)'
 ]
 
 const dim = {
@@ -35,14 +35,6 @@ const coordsHorario = [
     [asp1, 518, dim.aulasex[0], dim.aulasex[1]], //sexta
 
 ]
-
-Storage.prototype.setObject = function (key, value) {
-    this.setItem(key, JSON.stringify(value));
-}
-
-Storage.prototype.getObject = function (key) {
-    return JSON.parse(this.getItem(key));
-}
 
 function clock() { // We create a new Date object and assign it to a variable called "time".
     var time = new Date(),
@@ -94,88 +86,101 @@ if (0 <= h && h < 6) {
     r = 'Boa madrugada'
 }
 
-imagem = (i) => {
-    if (i == 'aulas') {
-        return 'horarioescola.jpeg'
-    }
-    if (i == 'plantoes') {
-        return 'horarioplantoes.jpeg'
-    }
-    if (i == 'aulasex') {
-        return 'horarioextra.png'
-    }
-}
-
-dia = (i, num) => {
-    pInt = (a, b) => {
-        return parseInt(coordsHorario[a][b])
-    }
-    if (i == 'aulas') {
-        switch (n) {
-            case 'segunda-feira':
-                return pInt(0, num)
-            case 'terça-feira':
-                return pInt(1, num)
-            case 'quarta-feira':
-                return pInt(2, num)
-            case 'quinta-feira':
-                return pInt(3, num)
-            case 'sexta-feira':
-                return pInt(4, num)
-            case 'sábado':
-                return pInt(5, num)
-            case 'domingo':
-                return 'semaula.png'
-        }
-    }
-    if (i == 'plantoes') {
-        switch (n) {
-            case 'segunda-feira':
-                return pInt(6, num)
-            case 'terça-feira':
-                return pInt(7, num)
-            case 'quarta-feira':
-                return pInt(8, num)
-            case 'quinta-feira':
-                return pInt(9, num)
-            case 'sexta-feira':
-                return pInt(10, num)
-            case 'sábado':
-                return 'semaula.png'
-            case 'domingo':
-                return 'semaula.png'
-        }
-    }
-    if (i == 'aulasex') {
-        switch (n) {
-            case 'segunda-feira':
-                return pInt(11, num)
-            case 'terça-feira':
-                return pInt(12, num)
-            case 'quarta-feira':
-                return pInt(13, num)
-            case 'quinta-feira':
-                return pInt(14, num)
-            case 'sexta-feira':
-                return pInt(15, num)
-            case 'sábado':
-                return 'semaula.png'
-            case 'domingo':
-                return 'semaula.png'
-        }
-    }
-}
-
-cf = (str) => {
-    if (((n == 'sábado' || n == 'domingo') && (str == 'aulasex' || str == 'plantoes')) || (n== 'domingo' && (str == 'aulas'))) {
-        return 'semaula.png'
-    } else {
-    cortarImagem(dia(str, 0), dia(str, 1), dia(str, 2), dia(str, 3), imagem(str))
-    }
-}
-
 const tit = document.querySelector('#ptit')
 tit.innerHTML = `${r}, hoje é ${n}!`
+
+cf = (str) => {
+    let dia = (i, num) => {
+        pInt = (a, b) => {
+            return parseInt(coordsHorario[a][b])
+        }
+        if (i == 'aulas') {
+            switch (n) {
+                case 'segunda-feira':
+                    return pInt(0, num)
+                case 'terça-feira':
+                    return pInt(1, num)
+                case 'quarta-feira':
+                    return pInt(2, num)
+                case 'quinta-feira':
+                    return pInt(3, num)
+                case 'sexta-feira':
+                    return pInt(4, num)
+                case 'sábado':
+                    return pInt(5, num)
+                case 'domingo':
+                    return 'semaula.png'
+            }
+        }
+        if (i == 'plantoes') {
+            switch (n) {
+                case 'segunda-feira':
+                    return pInt(6, num)
+                case 'terça-feira':
+                    return pInt(7, num)
+                case 'quarta-feira':
+                    return pInt(8, num)
+                case 'quinta-feira':
+                    return pInt(9, num)
+                case 'sexta-feira':
+                    return pInt(10, num)
+                case 'sábado':
+                    return 'semaula.png'
+                case 'domingo':
+                    return 'semaula.png'
+            }
+        }
+        if (i == 'aulasex') {
+            switch (n) {
+                case 'segunda-feira':
+                    return pInt(11, num)
+                case 'terça-feira':
+                    return pInt(12, num)
+                case 'quarta-feira':
+                    return pInt(13, num)
+                case 'quinta-feira':
+                    return pInt(14, num)
+                case 'sexta-feira':
+                    return pInt(15, num)
+                case 'sábado':
+                    return 'semaula.png'
+                case 'domingo':
+                    return 'semaula.png'
+            }
+        }
+    }
+
+    let imagem = (i) => {
+        if (i == 'aulas') {
+            return 'horarioescola.jpeg'
+        }
+        if (i == 'plantoes') {
+            return 'horarioplantoes.jpeg'
+        }
+        if (i == 'aulasex') {
+            return 'horarioextra.png'
+        }
+    }
+
+    let cortarImagem = (coord1, coord2, coord3, coord4, imagem) => {
+        Jimp.read(imagem).then(function (lenna) {
+            lenna.crop(coord1, coord2, coord3, coord4)
+            lenna.getBase64(Jimp.MIME_JPEG, function (err, src) {
+                var img = document.querySelector('#img1');
+                img.setAttribute("src", src);
+            });
+        }).catch(function (err) {
+            console.error(err);
+        });
+    }
+
+    if (((n == 'sábado' || n == 'domingo') && (str == 'aulasex' || str == 'plantoes')) || (n == 'domingo' && (str == 'aulas'))) {
+        return 'semaula.png'
+    } else {
+        cortarImagem(dia(str, 0), dia(str, 1), dia(str, 2), dia(str, 3), imagem(str))
+    }
+
+}
 
 const img1 = document.querySelector('#img1')
 
@@ -190,14 +195,18 @@ bAulas.onclick = () => {
 
 bAulasEx.onclick = () => {
     img1.setAttribute('src', cf('aulasex'))
-    if(cf('aulasex') != 'semaula.png') {img1.setAttribute('style', 'width:500px; height: 300px')} else {
+    if (cf('aulasex') != 'semaula.png') {
+        img1.setAttribute('style', 'width:500px; height: 300px')
+    } else {
         img1.setAttribute('style', 'width:400px;')
     }
 }
 
 bPlantoes.onclick = () => {
     img1.setAttribute('src', cf('plantoes'))
-    if(cf('plantoes') != 'semaula.png') {img1.setAttribute('style', 'width:500px; height: 300px')} else {
+    if (cf('plantoes') != 'semaula.png') {
+        img1.setAttribute('style', 'width:500px; height: 300px')
+    } else {
         img1.setAttribute('style', 'width:400px;')
     }
 }
@@ -209,31 +218,14 @@ renderTarefas = () => {
         let p = document.createElement('div')
         let inputcheckbox = document.createElement('input')
         inputcheckbox.setAttribute('type', 'checkbox')
+        inputcheckbox.setAttribute('class', 'save-cb-state')
         p.setAttribute('class', 'tarefa')
         let text = document.createTextNode(tarefas[item])
         p.appendChild(inputcheckbox)
         p.appendChild(text)
         divTarefa.appendChild(p)
-
     }
 }
 
-let cortarImagem = (coord1, coord2, coord3, coord4, imagem) => {
-    var file = 'new_name.' + 'png'
-    Jimp.read(imagem).then(function (lenna) {
-        lenna.crop(coord1, coord2, coord3, coord4)
-
-            .getBase64(Jimp.MIME_JPEG, function (err, src) {
-                var img = document.querySelector('#img1');
-                img.setAttribute("src", src);
-
-            });
-        lenna.write('./sextaaaaaa.png')
-    }).catch(function (err) {
-        console.error(err);
-    });
-}
-
-img1.setAttribute('src', cf('aulas'))
-
 renderTarefas()
+img1.setAttribute('src', cf('aulas'))
